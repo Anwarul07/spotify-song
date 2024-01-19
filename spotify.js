1b5e5a707fb7b34b1dd192c1826e975ffff1b020
Anwarul07 <mdanwarul064@gmail.com>
2024-01-19 21:49:17 +0530

    add
    added

--- spotify.js
+++ spotify.js
@@ -6,33 +6,30 @@ let prev = document.querySelector("#prev");
 let gif = document.querySelector("#gif");
 let range = document.querySelector("#range");
 
-
-
-
 let audio = new Audio("Spotify Files/songs/1.mp3");
 
 play.addEventListener("click", () => {
-    console.log("play cliked");
-    if (audio.paused || audio <= 0) {
-        audio.play();
-        play.classList.remove("fa-play-circle");
-        play.classList.add("fa-pause-circle");
-        gif.style.opacity = 1;
-    } else {
-        audio.pause();
-        play.classList.remove("fa-pause-circle");
-        play.classList.add("fa-play-circle");
-        gif.style.opacity = 0;
-
-    }
+  console.log("play cliked");
+  if (audio.paused || audio <= 0) {
+    audio.play();
+    play.classList.remove("fa-play-circle");
+    play.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audio.pause();
+    play.classList.remove("fa-pause-circle");
+    play.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
 });
 
 audio.addEventListener("timeupdate", () => {
+  console.log("time upadted");
+  progress = parseInt((audio.currentTime / audio.duration) * 100);
+  range.value = progress;
+  console.log("progressing time.......");
+});
 
-    console.log("time upadted");
-    progress = parseInt((audio.currentTime / audio.duration) * 100);
-    range.value = progress;
-    console.log("progressing time.......")
-
-})
-
+range.addEventListener("change", () => {
+  audio.currentTime = (range.value * audio.duration) / 100;
+});
