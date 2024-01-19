2f2c25d7acb5af644d3256274cb384e43d12fd2c
Anwarul07 <mdanwarul064@gmail.com>
2024-01-19 18:21:37 +0530

    Start
    Start

---
+++ spotify.js
@@ -0,0 +1,38 @@
+console.log("welcome to js of spotify");
+
+let play = document.querySelector("#play");
+let next = document.querySelector("#next");
+let prev = document.querySelector("#prev");
+let gif = document.querySelector("#gif");
+let range = document.querySelector("#range");
+
+
+
+
+let audio = new Audio("Spotify Files/songs/1.mp3");
+
+play.addEventListener("click", () => {
+    console.log("play cliked");
+    if (audio.paused || audio <= 0) {
+        audio.play();
+        play.classList.remove("fa-play-circle");
+        play.classList.add("fa-pause-circle");
+        gif.style.opacity = 1;
+    } else {
+        audio.pause();
+        play.classList.remove("fa-pause-circle");
+        play.classList.add("fa-play-circle");
+        gif.style.opacity = 0;
+
+    }
+});
+
+audio.addEventListener("timeupdate", () => {
+
+    console.log("time upadted");
+    progress = parseInt((audio.currentTime / audio.duration) * 100);
+    range.value = progress;
+    console.log("progressing time.......")
+
+})
+
