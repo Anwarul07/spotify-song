124f4eb10b4d04f576913bf87e56ed2f6dea3b0f
Anwarul07 <mdanwarul064@gmail.com>
2024-01-20 16:51:40 +0530

    song
    song index

--- spotify.js
+++ spotify.js
@@ -8,6 +8,61 @@ let range = document.querySelector("#range");
 
 let audio = new Audio("Spotify Files/songs/1.mp3");
 
+let songindex = 0;
+
+let song = [
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/1.mp3",
+    coverPath: "Spotify Files/covers/1.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/2.mp3",
+    coverPath: "Spotify Files/covers/2.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/3.mp3",
+    coverPath: "Spotify Files/covers/3.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/4.mp3",
+    coverPath: "Spotify Files/covers/4.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/5.mp3",
+    coverPath: "Spotify Files/covers/5.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/6.mp3",
+    coverPath: "Spotify Files/covers/6.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/7.mp3",
+    coverPath: "Spotify Files/covers/7.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/8.mp3",
+    coverPath: "Spotify Files/covers/8.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/9.mp3",
+    coverPath: "Spotify Files/covers/9.jpg",
+  },
+  {
+    songName: "salame-e-ishq",
+    filePath: "Spotify Files/songs/10.mp3",
+    coverPath: "Spotify Files/covers/10.jpg",
+  },
+]; 
+
 play.addEventListener("click", () => {
   console.log("play cliked");
   if (audio.paused || audio <= 0) {
