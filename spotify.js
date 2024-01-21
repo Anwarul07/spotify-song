30f992f58813582e423a2e976d131ab37d61d168
Anwarul07 <mdanwarul064@gmail.com>
2024-01-21 22:37:32 +0530

    update
    updated list ready

--- spotify.js
+++ spotify.js
@@ -6,62 +6,64 @@ let prev = document.querySelector("#prev");
 let gif = document.querySelector("#gif");
 let range = document.querySelector("#range");
 
+let songitem = Array.from(document.getElementsByClassName("song"))
+
 let audio = new Audio("Spotify Files/songs/1.mp3");
 
 let songindex = 0;
 
-let song = [
+let songs = [
   {
-    songName: "salame-e-ishq",
+    songName: "Warriyo",
     filePath: "Spotify Files/songs/1.mp3",
     coverPath: "Spotify Files/covers/1.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Chalo",
     filePath: "Spotify Files/songs/2.mp3",
     coverPath: "Spotify Files/covers/2.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "DEAF KEV",
     filePath: "Spotify Files/songs/3.mp3",
     coverPath: "Spotify Files/covers/3.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Diffrent Heaven",
     filePath: "Spotify Files/songs/4.mp3",
     coverPath: "Spotify Files/covers/4.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "anji Heroes",
     filePath: "Spotify Files/songs/5.mp3",
     coverPath: "Spotify Files/covers/5.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Rabba",
     filePath: "Spotify Files/songs/6.mp3",
     coverPath: "Spotify Files/covers/6.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Sakhiyaan",
     filePath: "Spotify Files/songs/7.mp3",
     coverPath: "Spotify Files/covers/7.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Bhula Dena",
     filePath: "Spotify Files/songs/8.mp3",
     coverPath: "Spotify Files/covers/8.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Tumhari Kasam",
     filePath: "Spotify Files/songs/9.mp3",
     coverPath: "Spotify Files/covers/9.jpg",
   },
   {
-    songName: "salame-e-ishq",
+    songName: "Na Jaana",
     filePath: "Spotify Files/songs/10.mp3",
     coverPath: "Spotify Files/covers/10.jpg",
   },
-]; 
+];
 
 play.addEventListener("click", () => {
   console.log("play cliked");
@@ -88,3 +90,11 @@ audio.addEventListener("timeupdate", () => {
 range.addEventListener("change", () => {
   audio.currentTime = (range.value * audio.duration) / 100;
 });
+
+
+songitem.forEach( (ele, i)=>{
+  console.log( ele , i );
+  ele.getElementsByTagName("img")[0].src=songs[i].coverPath;
+  ele.getElementsByClassName("songName")[0].innerText= songs[i].songName;
+});
+
